c9ee73e574a59f2b68ce0f8db857a821457990a1
leepeggy0528 <peggy6088@gmail.com>
2021-06-30 21:39:24 +0800

    coffee

---
+++ 2D_YYD/Assets/script/boss.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class boss : MonoBehaviour
+{
+    #region 欄位
+    [Header("餐點位置")]
+    public Transform[] traMeals;
+    [Header("移動速度")]
+    public float speed = 0.2f;
+
+    /// <summary>
+    /// 客人的位置
+    /// </summary>
+    private Transform traCustomer;
+    #endregion
+
+    #region 事件
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 取得訂單
+    /// </summary>
+    /// <param name="indexOrder">訂單編號</param>
+    public void GetOrder(int indexOrder, Transform traCustomer)
+    {
+        print("訂單編號" + indexOrder);
+
+        this.traCustomer = traCustomer;
+        StartCoroutine(MoveToMeal(traMeals[indexOrder]));
+    }
+
+    /// <summary>
+    /// 前往訂單位置
+    /// </summary>
+    /// <param name="traMeal"></param>
+    private IEnumerator MoveToMeal(Transform traMeal)
+    {
+        yield return StartCoroutine(MoveToObject(traMeal));
+        yield return new WaitForSeconds(0.5f);
+        yield return StartCoroutine(MoveToCustomer(traCustomer));
+    }
+
+    /// <summary>
+    /// 前往客人位置
+    /// </summary>
+    /// <param name="traCustomer">客人</param>
+    private IEnumerator MoveToCustomer(Transform traCustomer)
+    {
+        yield return StartCoroutine(MoveToObject(traCustomer));
+        yield return new WaitForSeconds(0.5f);
+        traCustomer.GetComponent<customer>().GetMeal();
+    }
+
+    /// <summary>
+    /// 前往指定物件
+    /// </summary>
+    /// <param name="traget">指定物件</param>
+    private IEnumerator MoveToObject(Transform traget)
+    {
+        while (Vector3.Distance(transform.position, traget.position) >= 0.5f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, traget.position, speed);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+    #endregion
+}
---
+++ 2D_YYD/Assets/script/customer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamemanger : MonoBehaviour
+{
+    #region 欄位
+    [Header("訂單種類")]
+    public Sprite[] sprOrder;
+    [Header("金幣")]
+    public Sprite sprCoin;
+
+    /// <summary>
+    /// Order-image
+    /// </summary>
+    private Image imgOrder;
+
+    /// <summary>
+    /// order-button
+    /// </summary>
+    private Button btnOrder;
+
+    /// <summary>
+    /// ordernumber
+    /// </summary>
+    private int indexOrder;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private boss boss;
+    #endregion
+
+    #region 事件
+    // Start is called before the first frame update
+    void Start()
+    {
+        imgOrder = transform.GetChild(0).Find("order").GetComponent<Image>();
+        btnOrder = imgOrder.GetComponent<Button>();
+        btnOrder.onClick.AddListener(ClickOrder);
+        boss = GameObject.Find("老闆").GetComponent<boss>();
+
+        RandomOrder();
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 隨機訂單
+    /// </summary>
+     private void RandomOrder()
+    {
+        indexOrder = Random.Range(0, sprOrder.Length);
+        imgOrder.sprite = sprOrder[indexOrder];
+    }
+
+    /// <summary>
+    /// 點擊訂單
+    /// </summary>
+    private void ClickOrder()
+    {
+        boss.GetOrder(indexOrder, transform);
+    }
+
+    /// <summary>
+    /// 取得餐點
+    /// </summary>
+    public void GetMeal()
+    {
+        imgOrder.sprite = sprCoin;
+    }
+    #endregion
+}
