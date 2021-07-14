2c7a38cd98adb4603a67f203d6ba1f66d33d55dd
leepeggy0528 <peggy6088@gmail.com>
2021-07-14 21:37:48 +0800

    web move

--- 2D_YYD/Assets/script/customerMarager.cs
+++ 2D_YYD/Assets/script/customerMarager.cs
@@ -9,16 +9,22 @@ public class customerMarager : MonoBehaviour
     [Header("客人種類")]
     public GameObject[] objcustomer;
 
+    [Header("客人位置")]
+    public Transform[] pointcustomer;
+
     [Header("出發點位置")]
     public Transform pointdeparture;
     [Header("抵達點位置")]
-    public Transform pointarrival;
+    public Transform[] pointarrival;
+
+    [Header("客人")]
+    public customerdata customer;
 
     private int indexOrder;
 
     private GameObject imgOrder;
 
-    private float speed = 2f;
+    private float speed = 300f;
     private float firstSpeed;
     private float delta = 0.2f; // 誤差值
     private static int i = 0;
@@ -30,53 +36,89 @@ public class customerMarager : MonoBehaviour
     private void Start()
     {
         imgOrder = GameObject.Find("departure").GetComponent<GameObject>();
-        indexOrder = Random.Range(0, objcustomer.Length);
-        firstSpeed = Vector3.Distance(objcustomer[indexOrder].transform.position, pointarrival.transform.position) * speed;
+        Invoke("Startcustomer", 1f);
     }
-
     private void Update()
     {
-        SpawnCustomer(indexOrder);
     }
 
-    
+
     #endregion
 
     #region 方法
-    private float calculateNewSpeed()
+    private void Startcustomer()
     {
-        //因為每次移動都是 Obj 在移動，所以要取得 Obj 和 PathB 的距離
-        float tmp = Vector3.Distance(objcustomer[indexOrder].transform.position, pointarrival.transform.position);
-
-        //當距離為0的時候，就代表已經移動到目的地了。
-        if (tmp == 0)
-            return tmp;
-        else
-            return (firstSpeed / tmp);
+        print("開始生成");
+        StartCoroutine(SpawnCustomer());
     }
 
+    private IEnumerator SpawnCustomer()
+    {
+        for (int i = 0; i < customer.points.Length; i++)
+        {
+           switch (customer.points[i])
+            {
+                case customerdata.PointType.zero:
+                    indexOrder = Random.Range(0, objcustomer.Length);
+                    GameObject o0 = Instantiate(objcustomer[indexOrder], pointarrival[0].position, Quaternion.identity);
+                    Destroy(o0, 10f);
+                    break;
+                case customerdata.PointType.one:
+                    indexOrder = Random.Range(0, objcustomer.Length);
+                    GameObject o1 = Instantiate(objcustomer[indexOrder], pointarrival[1].position, Quaternion.identity);
+                    Destroy(o1, 10f);
+                    break;
+
+                case customerdata.PointType.two:
+                    indexOrder = Random.Range(0, objcustomer.Length);
+                    GameObject o2 = Instantiate(objcustomer[indexOrder], pointarrival[2].position, Quaternion.identity);
+                    Destroy(o2, 10f);
+                    break;
+                case customerdata.PointType.three:
+                    indexOrder = Random.Range(0, objcustomer.Length);
+                    GameObject o3 = Instantiate(objcustomer[indexOrder], pointarrival[3].position, Quaternion.identity);
+                    Destroy(o3, 10f);
+                    break;
+                case customerdata.PointType.four:
+                    indexOrder = Random.Range(0, objcustomer.Length);
+                    GameObject o4 = Instantiate(objcustomer[indexOrder], pointarrival[4].position, Quaternion.identity);
+                    Destroy(o4, 10f);
+                    break;
+            }
+
+            yield return new WaitForSeconds(4f);
+            
+        }
+        
+    }
 
     ///<summary>
-    ///間隔生成節點
-    /// </summary> cf
-    void SpawnCustomer(int indexOrder)
+    ///間隔生成顧客
+    /// </summary>
+    /*private IEnumerator SpawnCustomer(int indexOrder)
     {
-        GameObject oUp =Instantiate(objcustomer[indexOrder], pointdeparture.position, Quaternion.identity);
-        Transform tup = oUp.transform;
+        for (int i = 0; i < 60; i++)
+        {
+            indexOrder = Random.Range(0, objcustomer.Length);
+            pointOrder = Random.Range(0, pointarrival.Length);
+            GameObject oUp = Instantiate(objcustomer[indexOrder], pointdeparture.position, Quaternion.identity);
 
-        // 讓物體朝向目標點 
-        transform.LookAt(pointarrival.position);
+            // 讓物體朝向目標點 
+            transform.LookAt(pointarrival[pointOrder].position);
 
-        // 物體向前移動
-        transform.Translate(-speed * Time.deltaTime, 0, 0); ;
+            // 物體向前移動
+            oUp.transform.Translate(speed * Time.deltaTime, speed * Time.deltaTime, 0);
 
-        // 判斷物體是否到達目標點
-        if (pointarrival.position.x > tup.position.x - delta
-            && pointarrival.position.x < tup.position.x + delta
-            && pointarrival.position.y > tup.position.y - delta
-            && pointarrival.position.y < tup.position.y + delta)
-            i = (i + 1) % objcustomer.Length;
-    }
+            // 判斷物體是否到達目標點
+            if (pointarrival[pointOrder].position.x > pointcustomer[indexOrder].position.x - delta
+                && pointarrival[pointOrder].position.x < pointcustomer[indexOrder].position.x + delta
+                && pointarrival[pointOrder].position.y > pointcustomer[indexOrder].position.y - delta
+                && pointarrival[pointOrder].position.y < pointcustomer[indexOrder].position.y + delta)
+                i = (i + 1) % objcustomer.Length;
+        }
+        yield return new WaitForSeconds(2f); //等待秒數
+
+    }*/
 
     #endregion
 }
---
+++ 2D_YYD/Assets/script/customerdata.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "peggy/customerdata", fileName = "CustomerData")]
+public class customerdata : ScriptableObject
+{
+    [Header("音樂節點")]
+    public PointType[] points;
+    public enum PointType
+    {
+       one,two,three,four,zero
+    }
+}
